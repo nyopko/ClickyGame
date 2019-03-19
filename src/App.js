563b323156a6036ef054e1701749de0af6e9a699
Alberto Balsalm <44181737+nyopko@users.noreply.github.com>
2019-03-19 11:42:16 -0400

    Regorganized folders, got test title text to appear.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+import Title from "./components/Title";
+
+
+class App extends Component {
+
+render(){
+    return(
+        <Title></Title>
+    )
+}
+}
+
+export default App;
