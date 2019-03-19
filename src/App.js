7405b30c464ef13186eda1adf0183ffdeb71cf0d
Alberto Balsalm <44181737+nyopko@users.noreply.github.com>
2019-03-19 12:31:42 -0400

    Pics now shuffle on click

--- src/App.js
+++ src/App.js
@@ -8,15 +8,47 @@ import Wrapper from "./components/Wrapper"
 class App extends Component {
     state = {
         friends,
+        clickedPics: [],
+        score:0,
+        goal: 9,
+        status:"",
     };
+
+    shufflePics = id => {
+        let clickedPics = this.state.clickedPics;
+
+        if(clickedPics.includes(id)){
+            this.setState({clickedPics: [], score: 0, status: "You lose!"});
+            return;
+        } else{
+            clickedPics.push(id)
+
+            if(clickedPics.length === 9){
+                this.setState({score: 9, status: "You win!", clickedPics: []})
+                return;
+            }
+            this.setState({friends, clickedPics, score: clickedPics.length, status:""});
+
+            for (let i = friends.length -1; i> 0; i--) {
+                let j = Math.floor(Math.random() * (i+1));
+                [friends[i], friends[j]] = [friends[j], friends[i]];
+            }
+        }
+    }
 
 render(){
     return(
         <div>
-        <Title></Title>
+        <Title
+        total={this.state.score}
+        goal={9}
+        status={this.state.status}
+        />
+        
         <Wrapper>
         {this.state.friends.map(friends => (
             <Card
+            shufflePics = {this.shufflePics}
                 id={friends.id}
                 key={friends.id}
                 image={friends.image}
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -3,7 +3,7 @@ import "./style.css";
 
 const Card = props => (
     <div className="imgCard">
-    <img alt={props.name} src={props.image} id={props.id}></img>
+    <img alt={props.name} src={props.image} id={props.id}  onClick = {() => props.shufflePics(props.id)} className='shufflePics'/>/>
 
     </div>
 )
