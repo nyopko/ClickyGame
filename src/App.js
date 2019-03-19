4d5536d42860b958fec3f2fbbb786bcfbad463a4
Alberto Balsalm <44181737+nyopko@users.noreply.github.com>
2019-03-19 13:59:58 -0400

    Formatted and cleaned up code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import Title from "./components/Title";
 import Card from "./components/Card";
 import friends from "./friends.json";
@@ -9,55 +9,55 @@ class App extends Component {
     state = {
         friends,
         clickedPics: [],
-        score:0,
+        score: 0,
         goal: 12,
-        message:"",
+        message: "",
     };
 
     shufflePics = id => {
         let clickedPics = this.state.clickedPics;
 
-        if(clickedPics.includes(id)){
-            this.setState({clickedPics: [], score: 0, message: "You lose!"});
+        if (clickedPics.includes(id)) {
+            this.setState({ clickedPics: [], score: 0, message: "You lose!" });
             return;
-        } else{
+        } else {
             clickedPics.push(id)
 
-            if(clickedPics.length === 12){
-                this.setState({score: 12, message: "You win! Click on another picture to play again!", clickedPics: []})
+            if (clickedPics.length === 12) {
+                this.setState({ score: 12, message: "You win! Click on another picture to play again!", clickedPics: [] })
                 return;
             }
-            this.setState({friends, clickedPics, score: clickedPics.length, message:""});
+            this.setState({ friends, clickedPics, score: clickedPics.length, message: "" });
 
-            for (let i = friends.length -1; i> 0; i--) {
-                let j = Math.floor(Math.random() * (i+1));
+            for (let i = friends.length - 1; i > 0; i--) {
+                let j = Math.floor(Math.random() * (i + 1));
                 [friends[i], friends[j]] = [friends[j], friends[i]];
             }
         }
     }
 
-render(){
-    return(
-        <div>
-        <Title
-        total={this.state.score}
-        goal={12}
-        message={this.state.message}
-        />
-        
-        <Wrapper>
-        {this.state.friends.map(friends => (
-            <Card
-            shufflePics = {this.shufflePics}
-                id={friends.id}
-                key={friends.id}
-                image={friends.image}
-            />
+    render() {
+        return (
+            <div>
+                <Title
+                    total={this.state.score}
+                    goal={12}
+                    message={this.state.message}
+                />
+
+                <Wrapper>
+                    {this.state.friends.map(friends => (
+                        <Card
+                            shufflePics={this.shufflePics}
+                            id={friends.id}
+                            key={friends.id}
+                            image={friends.image}
+                        />
                     ))}
-                    </Wrapper>
-        </div>
-    )
-}
+                </Wrapper>
+            </div>
+        )
+    }
 }
 
 export default App;
