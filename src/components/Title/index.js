85359f24fa44242597c265381f45bf6da2f08023
Alberto Balsalm <44181737+nyopko@users.noreply.github.com>
2019-03-19 13:26:13 -0400

    Added new images and info message

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -4,6 +4,7 @@ import "./style.css";
 function Title(props) {
     return <div className="gameText">
     <h1 className="title">The Click Game</h1>
+    <h4 className="info"> Try to select all 12 pictures without clicking the same one twice! </h4>
     <h3 className="score">Your score: {props.total}</h3>
         <h3 className="message">{props.message}</h3>
     
